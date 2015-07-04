56b9dac077d2f5c019f317cdee6b1b6a322f5591
higashi <higashi@siu.co.jp>
2015-07-05 01:09:01 +0900

    finish 6.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
+# セキュアパスワードの適用
 class User < ActiveRecord::Base
+	has_secure_password
 	before_save { self.email = email.downcase }
 
 	# name 必須チェック MAX 50文字
@@ -6,16 +8,13 @@ class User < ActiveRecord::Base
 		,presence: true \
 		,length: { maximum: 50 }
 
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z]+)+*\.[a-z]+\z/i
 
 	# email必須チェック
 	validates :email \
 		,presence: true \
 		,format: { with: VALID_EMAIL_REGEX } \
 		,uniqueness: { case_sensitive: false }
-	
-	# セキュアパスワードの適用
-	has_secure_password
 	
 	# パスワード検証
 	validates :password \
