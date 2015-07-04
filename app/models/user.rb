487a70446177e45fe2758adeb73726495bc3bda8
higashi <higashi@siu.co.jp>
2015-07-04 23:37:00 +0900

    finish 6.2

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+
+	# name 必須チェック MAX 50文字
+	validates :name \
+		,presence: true \
+		,length: { maximum: 50 }
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	# email必須チェック
+	validates :email \
+		,presence: true \
+		,format: { with: VALID_EMAIL_REGEX } \
+		,uniqueness: { case_sensitive: false }
+end
