49108a1e08e27aefe9e2b09c67af4f40f22937ef
higashi <higashi@siu.co.jp>
2015-07-06 22:51:19 +0900

    finish 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,28 +4,50 @@ class SessionsController < ApplicationController
 	
 	end
 	
-	
+	# form_for版
+#	def create
+#	
+#		user = User.find_by( email: params[:session][:email].downcase )
+#		
+#		if		user \
+#			&&	user.authenticate( params[:session][:password] )
+#			
+#			# ユーザをサインインさせ、ユーザページ(show)にリダイレクトする
+#			sign_in user
+#			
+#			redirect_to user
+#	
+#		else
+#		
+#			# エラーメッセージを表示し、サインインフォームを再描画する
+#			flash.now[:error] = 'Invalid email/password conbination'
+#			
+#			render 'new'
+#		end
+#	
+#	end
+
+	# form_tag版
 	def create
 	
-		user = User.find_by( email: params[:session][:email].downcase )
+		user = User.find_by( email: params[:email].downcase )
 		
-		if		user \
-			&&	user.authenticate( params[:session][:password] )
-			
-			# ユーザをサインインさせ、ユーザページ(show)にリダイレクトする
+		if	user && user.authenticate( params[:password] )
+		
 			sign_in user
 			
 			redirect_to user
-	
+		
 		else
 		
-			# エラーメッセージを表示し、サインインフォームを再描画する
 			flash.now[:error] = 'Invalid email/password conbination'
-			
+		
 			render 'new'
+		
 		end
 	
 	end
+
 	
 	
 	def destroy
