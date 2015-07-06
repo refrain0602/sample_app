5ca2a2edc2432a21070a07cdc7ea9906dd23d330
higashi <higashi@siu.co.jp>
2015-07-07 00:33:18 +0900

    finish 9.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user,	only: [:edit, :update]
+	before_action :correct_user,	only: [:edit, :update]
+
   def show
   
   	@user = User.find( params[:id] )
@@ -29,11 +32,45 @@ class UsersController < ApplicationController
   	
   end
   
+  def edit
+  
+#  	@user = User.find( params[ :id ] )
+  
+  end
+  
+  def update
+  
+#  	@user = User.find( params[ :id ] )
+  	
+  	if @user.update_attributes( user_params )
+  		# 更新に成功した場合を扱う
+  		flash[:success] = "Profile updated"
+  		
+  		redirect_to @user
+  	else
+  		render 'edit'
+  	end
+  
+  end
+  
 	# 内部処理群
 	private
   
 		def user_params
 			params.require( :user ).permit( :name, :email, :password, :password_confirmation )
 		end
-  
+		
+		# Before actions
+		
+		def signed_in_user
+		
+			redirect_to signin_url, notice: "Please sign in." unless signed_in?
+		
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+
+			redirect_to(root_path) unless current_user?(@user)
+		end
 end
