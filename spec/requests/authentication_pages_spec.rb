9ac33f5004c5abeb361678b949a416d4bca629c1
higashi <higashi@siu.co.jp>
2015-07-09 01:07:00 +0900

    finish 9.3.3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -52,11 +52,11 @@ describe "AuthenticationPages" do
 #			end
 			
 			it { should have_title(user.name) }
+			it { should have_link('Users',			href: users_path ) }
 			it { should have_link('Profile',     href: user_path(user)) }
 			it { should have_link('Settings',    href: edit_user_path(user)) }
 			it { should have_link('Sign out',    href: signout_path) }
 			it { should_not have_link('Sign in', href: signin_path) }
-		
 		
 			describe "followd by signout" do
 				before { click_link "Sign out" }
@@ -78,6 +78,13 @@ describe "AuthenticationPages" do
 					before { visit edit_user_path( user ) }
 					
 					it { should have_title('Sign in') }
+					
+					describe "visiting the user index" do
+						before { visit users_path }
+						
+						it { should have_title('Sign in') }
+					
+					end
 					
 				end
 				
@@ -110,4 +117,5 @@ describe "AuthenticationPages" do
 		end
 	
 	end
+	
 end
