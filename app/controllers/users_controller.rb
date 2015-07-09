b8bd0a18a70f215a5454072d7d71e4296a85b50f
higashi <higashi@siu.co.jp>
2015-07-10 01:38:03 +0900

    Finish user edit, update, index, and destory actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user,	only: [:index, :edit, :update]
+	before_action :signed_in_user,	only: [:index, :edit, :update, :destroy]
 	before_action :correct_user,	only: [:edit, :update]
+	before_action :admin_user,		only: :destroy
 
 	def index
 		@users = User.paginate( page: params[ :page ] )
@@ -57,6 +58,12 @@ class UsersController < ApplicationController
   
   end
   
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed."
+		redirect_to users_url
+	end
+  
 	# 内部処理群
 	private
   
@@ -77,4 +84,9 @@ class UsersController < ApplicationController
 
 			redirect_to(root_path) unless current_user?(@user)
 		end
+		
+	# adminじゃない場合はルート画面へ遷移
+	def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,8 +16,20 @@ describe User do
 	it { should respond_to( :password_confirmation ) }
 	it { should respond_to( :remember_token ) }
 	it { should respond_to( :authenticate ) }
+	it { should respond_to( :admin ) }
 
 	it { should be_valid }
+	it { should_not be_admin }
+	
+	# 権限チェック
+	describe "with admin attribute set to 'true'" do
+		before do
+			@user.save!
+			@user.toggle( :admin )
+		end
+		
+		it { should be_admin }
+	end
 	
 	# 永続トークンチェック
 	describe "remember token" do
